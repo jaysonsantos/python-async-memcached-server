1ddd4c5d209ebad48a278c9f4e65a0649c8b8856
Jayson Reis (jaysonsantos) <santosdosreis@gmail.com>
2014-10-15 14:06:15 -0300

    Add a setu.py

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+setup(
+    name='python-async-memcached-server',
+    version='0.0.1',
+    author='Jayson Reis',
+    author_email='santosdosreis@gmail.com',
+    description='A binary protocol memcached server written with twisted.',
+    url='https://github.com/jaysonsantos/python-async-memcached-server',
+    packages=['pmemcached'],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ]
+)
